732e88c446dc1aec8642b166f02db0e35d6865bc
Dana <dana.shaw95@gmail.com>
2024-12-13 15:06:54 -0600

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,19 +4,45 @@ const bcrypt = require("bcryptjs");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
 const router = express.Router();
 
+const validateSignup = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  check("firstName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide your first name."),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide your first name."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
+  handleValidationErrors,
+];
+
 // Sign up
-router.post("/", async (req, res) => {
+router.post("/", validateSignup, async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({ email, firstName, lastName, hashedPassword });
+  const user = await User.create({
+    email,
+    firstName,
+    lastName,
+    hashedPassword,
+  });
 
   const safeUser = {
     id: user.id,
     email: user.email,
     firstName: user.firstName,
-    lastName: user.lastName
+    lastName: user.lastName,
   };
 
   await setTokenCookie(res, safeUser);
