5bbfdb1f9c78721e39aee258a5e409ae23fe0603
Dana <dana.shaw95@gmail.com>
2025-01-07 18:03:43 -0600

    Frontend troubleshooting

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,22 +11,22 @@ const router = express.Router();
 // Serve React build files in production
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.join("../frontend/dist")));
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, "../frontend", "dist", "index.html")
+      path.join(__dirname, "../../frontend/dist/index.html")
     );
   });
 
-  // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/dist")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, "../frontend", "dist", "index.html")
+      path.join(__dirname, "../../frontend/dist/index.html")
     );
   });
 }
